4abf72d8b7beabf86e05d0cfd5aff36e58bb6bd1
Bhargavi Kommineni <bkommineni@dons.usfca.edu>
2018-04-26 07:09:14 +0000

    working code with reservoir sampling

--- example/client.go
+++ example/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
         "fmt"
+	"math/rand"
         "log"
         "encoding/gob"
         "net"
@@ -17,32 +18,37 @@ type SensorData struct {
 var batch_counter int = 0
 
  func main() {
+   rand.Seed(42)
    //in seconds give inerval to send data
-   var interval_to_send_data time.Duration = 5
-   readings := make([]SensorData,0)
+   var interval_to_send_data time.Duration =  3600
+   //readings := make([]SensorData,0)
+   var reservoir [4000]SensorData//reservoir
+   var i = 0
    ticker := time.NewTicker(time.Second * interval_to_send_data)
 
    go func() {
       for t := range ticker.C {
         _ = t
 
-        fmt.Println("Client")
-        fmt.Println("Start Client")
-        conn, err := net.Dial("tcp", "localhost:8081")
+        //fmt.Println("Client")
+        //fmt.Println("Start Client")
+        conn, err := net.Dial("tcp", "localhost:8085")
         if err != nil {
            log.Fatal("Connection error", err)
         }
         batch_counter = batch_counter + 1
-        fmt.Println("Batch No: ",batch_counter,"No of entries: " , len(readings))
-        for _, reading := range readings {
-          fmt.Println("Temperature ",reading.Temperature,", Humidty ",reading.Humidity);
-        }
+        //fmt.Println("Batch No: ",batch_counter,"No of entries: " , len(reservoir))
+	//fmt.Println("Readings size: ",len(readings))
+        //for _, reading := range reservoir {
+          //fmt.Println("Temperature ",reading.Temperature,", Humidty ",reading.Humidity);
+        //}
         encoder := gob.NewEncoder(conn)
-        encoder.Encode(readings)
+        encoder.Encode(reservoir)
         conn.Close()
-        fmt.Println("Done sending to server");
+        //fmt.Println("Done sending to server");
 
-        readings = []SensorData{}
+        //readings = []SensorData{}
+	
     }
    }()
 
@@ -53,7 +59,21 @@ var batch_counter int = 0
              dht.ReadDHTxx(sensorType, 27,false)
      if err == nil {
        sensorReading := SensorData{Temperature:temperature,Humidity:humidity}
-       readings = append(readings,sensorReading)
+       //readings = append(readings,sensorReading)
+       if i < 50 {
+       reservoir[i] = sensorReading;
+	} else {
+		j := rand.Intn(i+1);
+		//fmt.Println("Random number\n",j)
+		if j >= 0 && j < 50 {
+			//fmt.Println("Replacing index in reservoir with reading",j,sensorReading);
+			//fmt.Print(sensorReading);
+			reservoir[j] = sensorReading;
+		}
+	}
+	i++;
+       //fmt.Println(reservoir);
+       //fmt.Println(readings);
      }
    }
 
--- example/server.go
+++ example/server.go
@@ -2,7 +2,12 @@ package main
 
 import (
    "fmt"
+   "strconv"
    "net"
+   "time"
+   "log"
+   //"io/ioutil"
+   "os"
    "encoding/gob"
 )
 
@@ -15,14 +20,34 @@ var batch_counter int = 0
 
 func handleConnection(conn net.Conn) {
    fmt.Println("Connection Accepted!!")
-   var readings []SensorData
+   //var readings []SensorData
+   var reservoir [50]SensorData//reservoir
    dec := gob.NewDecoder(conn)
 
-   dec.Decode(&readings)
+   dec.Decode(&reservoir)
    batch_counter = batch_counter + 1
-   fmt.Println("Batch No: ",batch_counter,"No of entries: " , len(readings))
-   for _, reading := range readings {
-     fmt.Println("Temperature ",reading.Temperature,", Humidty ",reading.Humidity)
+   //fmt.Println("Batch No: ",batch_counter,"No of entries: " , len(reservoir))
+   current_time := time.Now().Local()
+    new_file, err := os.Create(current_time.Format("20060102150405") +".txt")
+    if err != nil {
+        log.Fatal("Cannot create file", err)
+    }
+    defer new_file.Close()
+   file, err := os.OpenFile(current_time.Format("20060102150405") +".txt", os.O_WRONLY|os.O_APPEND, 0644)
+    if err != nil {
+        log.Fatalf("failed opening file: %s", err)
+    }
+    defer file.Close()
+
+   for _, reading := range reservoir { 
+     //err := ioutil.WriteFile(current_time.Format("20060102150405") +".txt", []byte("Temperature : " + strconv.FormatFloat(float64(reading.Temperature), 'f', -1, 32) + " Humidity : " + strconv.FormatFloat(float64(reading.Humidity),'f',-1,32)),0600)
+	
+	len,err := file.WriteString("Temperature : " + strconv.FormatFloat(float64(reading.Temperature),     'f', -1, 32) + " Humidity : " + strconv.FormatFloat(float64(reading.Humidity),'f',-1,32) + "\n")
+	_ = len
+	if err != nil {
+            panic(err)
+     }
+     //fmt.Println("Temperature ",reading.Temperature,", Humidty ",reading.Humidity)
    }
 
    conn.Close()
@@ -30,7 +55,7 @@ func handleConnection(conn net.Conn) {
 
 func main() {
    fmt.Println("Starting Server.....")
-   ln, err := net.Listen("tcp", ":8081")
+   ln, err := net.Listen("tcp", ":8085")
    if err != nil {
       panic(err)
    }
